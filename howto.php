f95c4129c42fffce98177ee7dc6a6a8a7fdc0db0
miguel-aoyama <aoyamarikiya@gmail.com>
2021-12-21 14:39:59 +0900

    file change

--- howto.php
+++ howto.php
@@ -11,24 +11,29 @@
       <h3 class="logo">Re-cipe</h3>
     </header>
     <main>
-      <div class="container wrapper">
+      <div class="howto-container wrapper">
         <h2 class="howto-title">Re-cipeの使い方</h2>
         <div class="contents">
           <div class="content">
             <h3>1、レシピサイトからレシピをコピー</h3>
+            <img src="images/copy.png" alt="">
             <p>レシピサイトに載っているレシピをコピーしてきます。この時、先頭が『材料(○○人分)』となるようにしてください。</p>
           </div>
           <div class="content">
             <h3>2、コピーしたものをRe-cipeに貼り付ける</h3>
+            <img src="images/paste.jpg" alt="">
             <p>レシピサイトからコピーしたものをRe-cipeの入力欄に貼り付けます。</p>
           </div>
           <div class="content">
             <h3>3、作りたい人数を選択する</h3>
+            <img src="images/choose.jpg" alt="">
             <p>コピーを貼り付けたところの上に人数を選択する所があるので、そこで作りたい人数を選択します。</p>
           </div>
           <div class="content">
             <h3>『計算する』ボタンを押す</h3>
+            <img src="images/click.jpg" alt="">
             <p>計算すると書いてあるボタンを押すと、結果がもう一つの入力欄の方に表示されます。</p>
+            <img src="images/result.jpg" alt="">
           </div>
         </div>
       </div>
